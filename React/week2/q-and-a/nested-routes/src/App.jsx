48830d97da6107bfd39b4e5eb2f78670660b122c
Obada Moustafa Sharbatly <obadamoustafa@gmail.com>
2023-03-15 21:02:18 +0100

    nested routes

---
+++ React/week2/q-and-a/nested-routes/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React router dom</h1>
+        <h2>Thursday 16-03-2023</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
